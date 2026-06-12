7101aa351e2458614abb09a5aaf5bd6d7b9fff88
moyyikavya <moyyikavya593@gmail.com>
2026-06-12 13:48:00 +0530

    commit

---
+++ src/Services/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const api = axios.create({
+ baseURL: "https://travel-explorer-backend-5g9d.onrender.com"
+});
+
+export default api;
---
+++ src/features/favoriteSlice.js
@@ -0,0 +1,59 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const favoriteSlice = createSlice({
+  name: "favorites",
+
+  initialState:  JSON.parse(
+    localStorage.getItem(
+      "favorites"
+    )
+  ) || []
+,
+
+  reducers: {
+
+    addFavorite: (state, action) => {
+
+      const exists = state.find(
+        destination =>
+          destination.id === action.payload.id
+      );
+
+      if (!exists) {
+        state.push(action.payload);
+         localStorage.setItem(
+    "favorites",
+    JSON.stringify(state)
+  );
+
+      }
+
+    },
+
+    removeFavorite:(state,action)=>{
+
+ const updated =
+   state.filter(
+     destination =>
+     destination.id !==
+     action.payload
+   );
+
+ localStorage.setItem(
+   "favorites",
+   JSON.stringify(updated)
+ );
+
+ return updated;
+}
+
+
+  }
+});
+
+export const {
+  addFavorite,
+  removeFavorite
+} = favoriteSlice.actions;
+
+export default favoriteSlice.reducer;
